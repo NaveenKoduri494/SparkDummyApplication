e5030d18aa81b79294385d36761bc13592d5e8bc
NaveenKoduri494 <92677534+NaveenKoduri494@users.noreply.github.com>
2022-02-07 09:32:31 +0530

    Created using Colaboratory

---
+++ tricky_questions.py
@@ -0,0 +1,85 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled0.ipynb",
+      "provenance": [],
+      "collapsed_sections": [],
+      "authorship_tag": "ABX9TyM04MMiBqL2PcqEd9c2GCqw",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/NaveenKoduri494/SparkDummyApplication/blob/master/tricky_questions.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 1,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "KPq3hyMkUdiq",
+        "outputId": "20a86f96-5854-4099-f6cd-5a86e0ae2db2"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "[1, 2, 3, 4]\n"
+          ]
+        }
+      ],
+      "source": [
+        "l=[1,2,3,4]\n",
+        "print(l)"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "# update values in a dictionary\n",
+        "fruit = {\"Apple\":10,\"Banana\":30}\n",
+        "fruit[\"Apple\"] = 100\n",
+        "print(fruit)"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "gBl6XLyXUzCW",
+        "outputId": "e9fc3be0-4393-4c2a-a733-ef6882b9af1b"
+      },
+      "execution_count": 3,
+      "outputs": [
+        {
+          "output_type": "execute_result",
+          "data": {
+            "text/plain": [
+              "{'Apple': 100, 'Banana': 30}"
+            ]
+          },
+          "metadata": {},
+          "execution_count": 3
+        }
+      ]
+    }
+  ]
+}
